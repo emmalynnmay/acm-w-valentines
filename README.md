2b27902df4dfc10cb93bd7e1b69d0f7618d8fb05
emmalynnnn <emmaruthlynn@gmail.com>
2025-01-20 12:46:01 -0700

    Copying structure from other project

---
+++ README.md
@@ -0,0 +1,10 @@
+# USU ACM-W Valentines Box 2025
+
+Built with React (+ Typescript) in Vite
+
+* To run locally: npm run dev
+* To deploy: npm run deploy
+
+## Graphic Attribution
+* 
+
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import digger from './assets/digger.gif'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>USU ACM-W Valentines Box ❤️</h1>
+
+      <img className="logo" src={digger} alt="digger" />
+      <p>Construction in progress...</p>
+      <a className="credit" href="https://www.flaticon.com/free-animated-icons/excavator" title="excavator animated icons">Excavator animated icons created by Freepik - Flaticon</a>
+    </>
+  )
+}
+
+export default App
