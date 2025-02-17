177cf1ae4b25c39e66c6f6b7e6246551bfebcbc3
emmalynnnn <emmaruthlynn@gmail.com>
2025-02-17 15:27:32 -0700

    Emma May valentine

---
+++ src/components/pages/EmmaMay.jsx
@@ -0,0 +1,15 @@
+
+function EmmaMay() {
+    return (
+        <div className="emmamay-valentine-container">
+            {/* <div
+                className="emmamay-valentine-card"
+            >
+            <h1 className="emmamay-valentine-text">Happy Valentine's Ray! 💕</h1>
+            <img className="emmamay-img" src="https://i.pinimg.com/736x/b2/ae/5e/b2ae5e78e3e1c7fce111e2c1a58a3bf8.jpg"></img>
+            </div> */}
+    </div>
+    );
+}
+
+export default EmmaMay
